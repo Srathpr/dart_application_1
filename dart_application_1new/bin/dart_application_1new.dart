799dcd993ad1b02f26f0879f01a0f00a8ccad08b
sarath <prsarath672@gmail.com>
2022-07-12 14:03:59 +0530

    commit

---
+++ dart_application_1new/bin/dart_application_1new.dart
@@ -0,0 +1,5 @@
+import 'package:dart_application_1new/dart_application_1new.dart' as dart_application_1new;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_application_1new.calculate()}!');
+}
